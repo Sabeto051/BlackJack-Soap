a63787f1a5e3796265dd383d8539cabaf98ddc05
Sabeto051 <bedoya.santi@gmail.com>
2019-10-04 09:53:23 -0500

    prueba 2

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
+const soap = require('soap')
 const express = require('express')
 const path = require('path')
 
-const app = express()
+class Book {
+  constructor(name = '', year = 0) {
+    this.name = name
+    this.year = year
+  }
+}
+
+let myService = {
+  Library: {
+    BookLibrary: {
+      bookYear: args => {
+        let book = args.book
+        books = [
+          new Book('test 1', 2011),
+          new Book('test 2', 2012),
+          new Book('test 3', 2013)
+        ]
+
+        findBook = bookk => {
+          return bookk.name == book.name
+        }
+
+        return { year: books.find(findBook).year }
+      }
+    }
+  }
+}
+
+var xml = require('fs').readFileSync('Library.wsdl', 'utf8')
+
+var app = express()
 
 app.set('port', process.env.PORT || 3000)
 
 app.use(express.json())
 
-// app.get('/', (req, res) => {
-//   res.send('Hola mijo')
-// })
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.listen(app.get('port'), () => {
-  console.log(`server on port ${app.get('port')}`)
+app.listen(app.get('port'), function() {
+  soap.listen(app, '/Library', myService, xml, function() {
+    console.log(`server initialized on port ${app.get('port')}`)
+  })
 })
