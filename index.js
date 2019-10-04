b7321c506d15d1fea2ff245e584eb496bdff0073
Sabeto051 <bedoya.santi@gmail.com>
2019-10-04 09:42:28 -0500

    prueba 1

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-alert('Hola')
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.set('port', process.env.PORT || 3000)
+
+app.use(express.json())
+
+// app.get('/', (req, res) => {
+//   res.send('Hola mijo')
+// })
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.listen(app.get('port'), () => {
+  console.log(`server on port ${app.get('port')}`)
+})
