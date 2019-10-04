99c735926fdf84a3941507be103fac577fca65b4
Sabeto051 <bedoya.santi@gmail.com>
2019-10-04 11:15:41 -0500

    cliente async

--- cliente.js
+++ cliente.js
@@ -4,20 +4,41 @@ const soap = require('soap')
 const url = 'https://blackjack-soap-server.herokuapp.com/Library?wsdl'
 
 class Book {
-  constructor(name = '') {
+  constructor(name = '', year = 0) {
     this.name = name
+    this.year = year
   }
 }
 
 let book = new Book('test 2')
 
-soap.createClient(url, { disableCache: true }, function(err, client) {
-  if (err) throw err
-  console.log(client.describe().Library)
-  console.log(client.describe().Library.BookLibrary.bookYear)
+let clienteInstagram = null
 
-  client.bookYear({ book }, (err, res) => {
-    if (err) throw err
-    console.log(res)
-  })
-})
+async function getClienteInsta() {
+  if (clienteInstagram === null) {
+    try {
+      clienteInstagram = await soap.createClientAsync(url, {
+        disableCache: true
+      })
+
+      clienteInstagram.bookYear({ book }, async (err, res) => {
+        if (err) throw err
+        console.log(res)
+      })
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  return clienteInstagram
+}
+
+let aa = getClienteInsta()
+
+// aa.bookYear({ book }, async (err, res) => {
+//   if (err) throw err
+//   console.log(res)
+// })
+
+// module.exports = {
+//   getCliente: getClienteInsta()
+// }
