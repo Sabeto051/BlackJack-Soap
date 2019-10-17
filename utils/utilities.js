ed1b85429089eeea43cbea6acb4840f71293e752
Sabeto051 <bedoya.santi@gmail.com>
2019-10-17 00:56:44 -0500

    definicion metodos

--- utils/utilities.js
+++ utils/utilities.js
@@ -2,10 +2,11 @@ const tokenizator = require('crypto')
 
 let jugadores = []
 
-let serverStatus = {
-  juegoComenzado: false,
+let juegoStatus = {
+  Comenzado: false,
   jugadoresConectados: 0,
-  hayGanador: true
+  jugadorEnTurno: null, // posicion en array, casa es jugador -1
+  terminado: false
 }
 
 let casaStatus = {
@@ -20,12 +21,105 @@ function conectarJugador(apuesta = 0) {
     token,
     apuesta,
     numeroCartas: 0,
-    cartas: []
+    cartas: [],
+    perdio: false,
+    blackjack: false,
+    posicionEnArray: jugadores.length,
+    plantado: false
   }
   jugadores.push(player)
-  // console.log(jugadores)
+
+  juegoStatus.jugadoresConectados += 1
+  juegoStatus.juegoComenzado = true
+
+  // SE empieza el CONTEO
+
   return token
 }
+
+/* 
+  Conteo para que empiece el juego
+  cuando se acaba, o cancela porque hay 2 jugadores jugando
+  llama a empezarJuego()
+*/
+function conteoJuegoInicio() {}
+
+/* 
+  Reparte y crea las cartas de jugadores y casa
+*/
+function empezarJuego() {}
+
+/* 
+  Crea una carta (numero random 1-52)
+  server no pinta carta, lo hace el cliente
+*/
+function newCarta() {}
+
+/* 
+  Conteo de cartas
+*/
+function contarCartas() {}
+
+/* 
+  verifica si un jugador está en el turno actual
+*/
+function verificarJugadorEnTuno(token) {}
+
+/* 
+  valida que jugador juega despues, o si la casa juega despues
+  puede llama funcion jugarCasa()
+*/
+function siguienteTurno() {}
+
+/* 
+  juega la casa (despues de q todos jugadores jugaron)
+  Casa puede pedir mas cartas, se puede pasar en puntos, etc...
+*/
+function jugarCasa() {}
+
+/* 
+  Verifica quien Gana al FINAL y devuelve o se queda con la plata de las apuestas
+*/
+function verificarQuienGana() {}
+
+/* 
+  resetea todo, se envia a clientes si ganaron o perdieron
+  Se tiene que esperar 20 segundos mostranso los mensajes de fin
+  Luego del tiempo se inicia el proceso otra vez
+*/
+function terminarJuego() {}
+
+/* 
+  PETICION cliente
+  Jugador se planta y envia su token de verificacion
+  llama a verificarJugadorEnTuno para ver si es su turno
+*/
+function plantar(token) {}
+
+/* 
+  PETICION cliente
+  Jugador lo invoca y envia su token de verificacion
+  llama a verificarJugadorEnTuno para ver si es su turno
+*/
+function cartaCartaAdicional(token) {}
+
+/* 
+  PETICION GET cliente
+  Devuelve solo una carta de la casa, porque la otra es oculta
+*/
+function casaStatusRequest() {}
+
+/* 
+  PETICION GET cliente
+  Retorna el jugador en la pos del array requerida
+*/
+function jugadorStatusRequest(posicionEnArray) {}
+
+/* 
+  PETICION POST cliente
+  Retorna el server status
+*/
+function juegoStatusRequest() {}
 
 module.exports = {
   conectarJugador
